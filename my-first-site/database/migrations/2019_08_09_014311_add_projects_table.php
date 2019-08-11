0a708c5a0cb63a913a42765cd0673be786ed2ebc
André Luis Gomes <andrelugomes@gmail.com>
2019-08-11 16:48:25 -0300

    My First Laravel Site

---
+++ my-first-site/database/migrations/2019_08_09_014311_add_projects_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // add keyword dosen't exist
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
